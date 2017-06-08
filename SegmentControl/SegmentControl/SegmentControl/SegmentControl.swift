39141891afacf3123a5d91492cd53dc61d8add8b
Harris <hanhui@ngs.tech>
2017-06-08 21:56:21 +0800

    initial the project of segmentControl

---
+++ SegmentControl/SegmentControl/SegmentControl/SegmentControl.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentControl.swift
+//  SegmentControl
+//
+//  Created by Harris on 08/06/2017.
+//  Copyright © 2017 Harris. All rights reserved.
+//
+
+import UIKit
+
+class SegmentControl: UIControl {
+
+}
